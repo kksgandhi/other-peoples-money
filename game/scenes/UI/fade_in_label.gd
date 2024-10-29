051982b6844b7b79a1cfb76d6dcd08c43e1df375
Kutub Gandhi <kksgandhi@gmail.com>
2024-10-29 16:06:06 -0400

    [WIP] move messages to the center

--- game/scenes/UI/fade_in_label.gd
+++ game/scenes/UI/fade_in_label.gd
@@ -3,16 +3,17 @@ extends Control
 var has_triggered: bool = false
 
 @onready var label:RichTextLabel = (%Label as RichTextLabel)
+@onready var container:PanelContainer = (%Container as PanelContainer)
 @onready var animation_player:AnimationPlayer = (%AnimationPlayer as AnimationPlayer)
 @export var is_reflective_text := false
 
 func _ready() -> void:
-  label.visible = false
+  container.visible = false
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
   if has_triggered: return
   if is_reflective_text and Globals.hide_reflection_text: return
     
-  label.visible = true
+  container.visible = true
   animation_player.play("fade_in")
   has_triggered = true
